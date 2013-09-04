342ad2d0f152e3633f086d1a23b405080551c6a8
masted <masted311@gmail.com>
2013-09-04 16:23:04 +0400

    ...

---
+++ cmd/1.php
@@ -0,0 +1,10 @@
+<?php
+
+(new PmLocalServer([
+  'title' => 'Sample',
+  'name' => 'id9',
+  'type' => 'sd',
+  'domain' => 'id9.sitedraw.ru',
+  'active' => 1,
+  'userId' => 1
+]))->a_createProject();
--- lib/SdProjectItems.class.php
+++ lib/SdProjectItems.class.php
@@ -67,6 +67,7 @@ class SdProjectItems extends DdItems {
   }
 
   protected function createStat($id) {
+    //return;
     $item = $this->getItem($id);
     $db = new Db(DB_USER, DB_PASS, DB_HOST, 'stat');
     $statId = $db->insert('stat_site', [
