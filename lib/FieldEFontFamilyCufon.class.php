6208f0d72c81cc207033c9f08dbfdf17cce9450c
masted <masted311@gmail.com>
2014-12-25 09:58:09 +0300

    Auto-commit on 25.12.2014 10:58:08

--- lib/FieldEFontFamilyCufon.class.php
+++ lib/FieldEFontFamilyCufon.class.php
@@ -4,14 +4,13 @@ class FieldEFontFamilyCufon extends FieldEDialogSelect {
 
   static $title = 'Шрифт';
 
-  public $useTypeJs = true;
-
   protected function defineOptions() {
     $options['options'] = Html::defaultOption();
     foreach (glob(Sflm::$absBasePaths['sd'].'/js/fonts/*') as $v) {
       $v = basename(Misc::removeSuffix('.js', $v));
       $options['options'][$v] = $v;
     }
+    $options['useTypeJs'] = true;
     return array_merge(parent::defineOptions(), $options);
   }
 
--- lib/FieldESvgSelect.class.php
+++ lib/FieldESvgSelect.class.php
@@ -2,14 +2,13 @@
 
 class FieldESvgSelect extends FieldEDialogSelect {
 
-  public $useTypeJs = true;
-
   protected function defineOptions() {
     $options['options'] = Html::defaultOption();
     foreach (CtrlSdSvg::getFiles() as $v) {
       $name = basename(Misc::removeSuffix('.svg', $v));
       $options['options'][$name] = $name;
     }
+    $options['useTypeJs'] = true;
     return array_merge(parent::defineOptions(), $options);
   }
 
