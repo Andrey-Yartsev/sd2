67749fbd9ef7bd45bef06afea2ecabda50bb6b51
masted <masted311@gmail.com>
2016-03-28 11:58:59 +0300

    v2-release add fonts

---
+++ sd/js/fonts/Font_Awesome.js
@@ -0,0 +1,20 @@
+/*!
+ * The following copyright notice may not be removed under any circumstances.
+ * 
+ * Copyright:
+ * SIL Open Font License 1.1
+ * 
+ * Trademark:
+ * Please refer to the Copyright section for the font trademark attribution
+ * notices.
+ * 
+ * Manufacturer:
+ * Fort Awesome
+ * 
+ * Designer:
+ * Dave Gandy
+ * 
+ * Vendor URL:
+ * http://fontawesome.io
+ */
+Cufon.registerFont({"w":90,"face":{"font-family":"Font_Awesome","font-weight":400,"font-stretch":"normal","font-style":"normal","units-per-em":360,"panose-1":"0 0 0 0 0 0 0 0 0 0","ascent":309,"descent":-51,"bbox":"0 0 0 0","underline-thickness":"18.0804","underline-position":"-17.8795","unicode-range":"U+0020-0020"},"glyphs":{" ":{},"\u00a0":{}}});
