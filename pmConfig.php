a22ef9a573f2a8b5dfcedc19b67c9c79849e7c21
masted <masted311@gmail.com>
2016-03-29 12:07:45 +0300

    ...

--- pmConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-  'noDb'         => true,
-  'vhostAliases' => [
-    '/sd/' => '{ngnEnvPath}/bc-prod/sd/'
-  ],
-  'afterCmdTttt' => 'php {runPath}/run.php site {name} bc-prod/install'
-];
