2e39389d499e2b011c009dd2bd67468b300d124e
masted <masted311@gmail.com>
2016-06-23 14:32:43 +0300

    ...

--- sd/js/Ngn.sd.GlobalSlides.js
+++ sd/js/Ngn.sd.GlobalSlides.js
@@ -85,7 +85,9 @@ Ngn.sd.GlobalSlides = new Class({
     this.phantomFrameChange();
     this.cacheSlides();
     this.initMaxSlidesBlockN();
-    this.animationId = this.nextSlide.periodical(this.duration, this);
+    if (this.slides.length) {
+      this.animationId = this.nextSlide.periodical(this.duration, this);
+    }
   }
 
 });
