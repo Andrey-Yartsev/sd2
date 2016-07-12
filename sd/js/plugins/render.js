e94262788dc8f8513db47d857f276dd46f1800d0
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-07-12 19:44:16 +0300

    BC-6 render method refactoring

--- sd/js/plugins/render.js
+++ sd/js/plugins/render.js
@@ -1,13 +1,5 @@
-Ngn.sd.render = function() {
-  new Ngn.Dialog.HtmlPage({
-    url: url = '/render/' + Ngn.sd.bannerId,
-    title: 'Render',
-    width: Ngn.sd.bannerSize.w.toInt() + 30
-  });
-};
-
 window.addEvent('sdPanelComplete', function() {
   new Ngn.Btn(Ngn.sd.fbtn('Render', 'render'), function() {
-    Ngn.sd.render();
+    Ngn.sd.Render();
   });
 });
