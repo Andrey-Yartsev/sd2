e3cda394b950eac88e38bd05b29a9a144a39c519
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-07-15 15:24:57 +0300

    BC-7 bottom bar scroll implementation

--- lib/CtrlSdAllBanners.class.php
+++ lib/CtrlSdAllBanners.class.php
@@ -4,7 +4,7 @@ class CtrlSdAllBanners extends CtrlBase {
 
   function action_json_default() {
     $this->json['banners'] = [];
-    foreach (db()->select('SELECT * FROM bcBanners WHERE userId=?d LIMIT 5', Auth::get('id')) as $v) {
+    foreach (db()->select('SELECT * FROM bcBanners WHERE userId=?d', Auth::get('id')) as $v) {
       $this->json['banners'][] = (new BcBanner($v))->r;
     }
   }
