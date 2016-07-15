128401e39ca009f6bbc3b28fd34e9fbf19869207
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-07-15 13:36:28 +0300

    BC-18 add user access check on editing and rendering

--- lib/CtrlSdRenderAbstract.class.php
+++ lib/CtrlSdRenderAbstract.class.php
@@ -7,6 +7,8 @@ abstract class CtrlSdRenderAbstract extends CtrlCommon {
   }
 
   protected function render() {
+    $banner = db()->getRow('bcBanners', $this->req->param(1));
+    if ($banner['userId'] != Auth::get('id')) throw new AccessDenied;
     return O::di('BcRender', $this->req->param(1))->render();
   }
 
