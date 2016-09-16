839ac6d27d3b73604decb72b103795238da6a82c
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2016-09-16 14:09:44 +0300

    ...

--- config/vars/sfl/css/sd.php
+++ config/vars/sfl/css/sd.php
@@ -1,7 +1,12 @@
 <?php
 
 return [
-  'common',
+//  'common',
+  'i/css/common/screen.css',
+  'i/css/common/btns.css',
+  'i/css/common/dialog.css',
+  'i/css/common/form.css',
+  'i/css/rainbow/small.css',
   'content',
   'icons',
   '/sd/css/common.css',
