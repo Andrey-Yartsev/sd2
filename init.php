6491ccf67da51fb18eea40d360101e3ebfd3a2ef
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2017-03-22 08:08:10 -0400

    ...

--- init.php
+++ init.php
@@ -6,8 +6,3 @@ Config::addBasePath(dirname(NGN_PATH).'/projects/sitedraw/site/config', 3);
 O::replaceInjection('DefaultRouter', 'SdRouter');
 Sflm::$version = 9;
 Sflm::$absBasePaths['sd'] = __DIR__.'/sd';
-
-if (file_exists(NGN_ENV_PATH.'/config/database.php')) {
-  // default server config
-  require NGN_ENV_PATH.'/config/database.php';
-}
