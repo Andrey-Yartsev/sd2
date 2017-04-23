ce0c3ca846969bac65feed24016d0a82c206c1f4
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-04-23 20:57:35 +0300

    merge

--- init.php
+++ init.php
@@ -4,5 +4,4 @@ define('SD_PATH', __DIR__);
 Ngn::addBasePath(SD_PATH, 3);
 Config::addBasePath(dirname(NGN_PATH).'/projects/sitedraw/site/config', 3);
 O::replaceInjection('DefaultRouter', 'SdRouter');
-Sflm::$version = 9;
 Sflm::$absBasePaths['sd'] = __DIR__.'/sd';
